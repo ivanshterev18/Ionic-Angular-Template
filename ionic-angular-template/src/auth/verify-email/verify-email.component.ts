b0115b0621eff6b23e7d3d214c4b1d76a4b75112
IvanShterev <schterev11@gmail.com>
2020-03-26 10:12:04 +0200

    Added custom email template

---
+++ ionic-angular-template/src/auth/verify-email/verify-email.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-verify-email',
+  templateUrl: './verify-email.component.html',
+  styleUrls: ['./verify-email.component.scss'],
+})
+export class VerifyEmailComponent implements OnInit {
+  
+  public mode: string;
+  public actionCode: string;
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    // private notificationService: NotificationService,
+    private authService: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams
+      .subscribe(async (params) => {
+        if (!params) {
+          this.router.navigate(['/']);
+        }
+        this.mode = params.mode;
+        this.actionCode = params.oobCode;
+        try {
+            await this.authService.verifyEmail(this.actionCode);
+            // this.notificationService.success('Your email was verified !');
+            this.router.navigate(['signin']);
+          } catch (e) {
+            // this.notificationService.error(e);
+          }
+    });
+  }
+
+}
