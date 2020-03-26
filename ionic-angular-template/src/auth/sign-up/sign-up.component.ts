310e6b74c6fb7338c71b0e32f32f96e813b5dace
IvanShterev <schterev11@gmail.com>
2020-03-26 14:50:28 +0200

    Updated strings with ngx translate and set css class for auth forms

--- ionic-angular-template/src/auth/sign-up/sign-up.component.ts
+++ ionic-angular-template/src/auth/sign-up/sign-up.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,15 +11,20 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  private infoMessages: any;
   public signUpForm: FormGroup;
   constructor(
     private authService: AuthService,
     private router: Router,
     private alertCtrl: AlertController,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private tranlate: TranslateService
   ) { }
 
   ngOnInit() {
+    this.tranlate.get('Info-messages').subscribe((messages) => {
+      this.infoMessages = messages;
+    });
     this.signUpForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -48,7 +54,7 @@ export class SignUpComponent implements OnInit {
   private async showErrorAlert(message: string) {
     const alert = await this.alertCtrl
       .create({
-        header: 'Authentication failed',
+        header: `${this.infoMessages.authFailed}`,
         message: message,
         buttons: ['Okay']
       })
@@ -58,9 +64,9 @@ export class SignUpComponent implements OnInit {
   private async showInfoAlert() {
     const alert = await this.alertCtrl
       .create({
-        header: 'Info Message',
-        message: 'If you want to use all functionalities you have to verify your email first!',
-        buttons: ['Okay']
+        header: `${this.infoMessages.infoMessage}`,
+        message: `${this.infoMessages.message}`,
+        buttons: [`${this.infoMessages.button}`]
       })
       alert.present();
   }
