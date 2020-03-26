ef2df9fb52d7b6a565bf253a159185d8f8ed3a07
IvanShterev <schterev11@gmail.com>
2020-03-26 15:07:56 +0200

    refactoring

--- ionic-angular-template/src/auth/verify-email/verify-email.component.ts
+++ ionic-angular-template/src/auth/verify-email/verify-email.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.scss'],
 })
-export class VerifyEmailComponent implements OnInit {
+export class VerifyEmailComponent {
   
   public mode: string;
   public actionCode: string;
@@ -16,24 +16,22 @@ export class VerifyEmailComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     // private notificationService: NotificationService,
     private authService: AuthService,
-  ) { }
-
-  ngOnInit(): void {
+  ) { 
     this.activatedRoute.queryParams
-      .subscribe(async (params) => {
-        if (!params) {
-          this.router.navigate(['/']);
+    .subscribe(async (params) => {
+      if (!params) {
+        this.router.navigate(['/']);
+      }
+      this.mode = params.mode;
+      this.actionCode = params.oobCode;
+      try {
+          await this.authService.verifyEmail(this.actionCode);
+          // this.notificationService.success('Your email was verified !');
+          this.router.navigate(['signin']);
+        } catch (e) {
+          // this.notificationService.error(e);
         }
-        this.mode = params.mode;
-        this.actionCode = params.oobCode;
-        try {
-            await this.authService.verifyEmail(this.actionCode);
-            // this.notificationService.success('Your email was verified !');
-            this.router.navigate(['signin']);
-          } catch (e) {
-            // this.notificationService.error(e);
-          }
-    });
+  });
   }
 
 }
