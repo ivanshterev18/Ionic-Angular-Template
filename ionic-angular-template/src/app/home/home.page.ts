f75346d1fa165c69de3b3e531fcb91b9e701f863
IvanShterev <schterev11@gmail.com>
2020-03-31 10:47:15 +0300

    Home page completed

--- ionic-angular-template/src/app/home/home.page.ts
+++ ionic-angular-template/src/app/home/home.page.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  private loggedUserSubscription: Subscription;
+  public loggedUser: any;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    // private notificationService: NotificationService
+     ) {}
 
+  ngOnInit(): void {
+    this.loggedUserSubscription = this.authService.loggedUserData$.subscribe(async (data) => this.loggedUser = await data);
+  }
+  ngOnDestroy(): void {
+    if (this.loggedUserSubscription) {
+      this.loggedUserSubscription.unsubscribe();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    // this.notificationService.success('Successfully logout');
+    this.router.navigate(['/signin']);
+  }
 }
